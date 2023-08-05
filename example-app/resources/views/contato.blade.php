be2ea910994db5a7d66c7e78bffec41cfaa13f4c
Pietro Rosolia <pietro.rosolia.2704@gmail.com>
2023-08-05 00:46:44 -0300

    Funciona

--- example-app/resources/views/contato.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('title','Entre em Contato')
-
-@section('content')
-<h1>Página de Contato</h1>
-<p><a href="/">Voltar para Home</a></p>
-<p><a href="/products">Ir para Produtos</a></p>
-@endsection
